9d6059adcf23696e106bf1a3da584f4d540bf21c
leye195 <leye195@naver.com>
2023-02-07 23:12:53 +0900

    feat: create stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,124 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/widgets/button.dart';
-import 'package:toonflix/widgets/currency_card.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+// state
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onClicked() {
+    setState(() {
+      counter = counter + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            backgroundColor: const Color(0xff181818),
-            body: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(
-                        height: 80,
-                      ), //box with size
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: const [
-                              Text(
-                                'Hey, Selena',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 28,
-                                    fontWeight: FontWeight.w700),
-                              ),
-                              SizedBox(height: 4),
-                              Text('Welcome back',
-                                  style: TextStyle(
-                                      color: Colors.white70,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w300)),
-                            ],
-                          )
-                        ],
-                      ),
-                      const SizedBox(height: 60),
-                      const Text(
-                        'Total Balance',
-                        style: TextStyle(
-                            color: Colors.white70,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w300),
-                      ),
-                      const SizedBox(height: 4),
-                      const Text('\$5 194 382',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 40,
-                            fontWeight: FontWeight.w700,
-                          )),
-                      const SizedBox(height: 16),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: const [
-                          Button(
-                              text: 'Transfer',
-                              bgColor: Color(0xfff2b33a),
-                              textColor: Color(0xff000000)),
-                          Button(
-                              text: 'Request',
-                              bgColor: Color(0xff1f2123),
-                              textColor: Colors.white)
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 48,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: const [
-                          Text('Wallets',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.w600)),
-                          Text('View All',
-                              style: TextStyle(
-                                  color: Colors.white70,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w300)),
-                        ],
-                      ),
-                      const SizedBox(height: 20),
-                      const CurrencyCard(
-                          amount: '6 425',
-                          currency: 'EUR',
-                          name: 'Euro',
-                          symbol: Icons.euro_symbol,
-                          isInverted: false),
-                      const CurrencyCard(
-                        amount: '2 200',
-                        currency: 'BTC',
-                        name: 'Bitcoin',
-                        symbol: Icons.currency_bitcoin,
-                        isInverted: true,
-                        offset: -25,
-                      ),
-                      const CurrencyCard(
-                        amount: '55 203',
-                        currency: 'USD',
-                        name: 'Dollar',
-                        symbol: Icons.attach_money,
-                        isInverted: false,
-                        offset: -50,
-                      ),
-                    ]),
-              ),
-            )));
+      home: Scaffold(
+        body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const Text('Click Count',
+                style: TextStyle(
+                  fontSize: 32,
+                )),
+            Text('$counter', style: const TextStyle(fontSize: 30)),
+            IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: const Icon(
+                  Icons.add_box_rounded,
+                ))
+          ]),
+        ),
+      ),
+    );
   }
 }
