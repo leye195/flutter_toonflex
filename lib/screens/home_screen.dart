361f955b7ec6d21b0479486ac5f25ef4fdcd9cc8
leye195 <leye195@naver.com>
2023-02-12 15:08:30 +0900

    feat: add button section

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
+const timeList = [1500, 300, 900];
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -12,26 +14,64 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int totalCount = 0;
   int totalSeconds = 1500;
+  int activeIndex = 0;
   bool isRunning = false;
 
   late Timer timer;
 
+  // date format
+  String format(int seconds) {
+    var duration = Duration(seconds: seconds);
+    return duration.toString().split('.').first.substring(2);
+  }
+
+  void onTimeout() {
+    setState(() {
+      totalCount += 1;
+      totalSeconds = timeList[activeIndex];
+      isRunning = false;
+    });
+  }
+
   void onTick(Timer timer) {
+    if (totalSeconds == 0) {
+      onTimeout();
+      timer.cancel();
+      return;
+    }
+
     setState(() {
       totalSeconds -= 1;
     });
   }
 
+  dynamic onTabPress(int idx) {
+    return () {
+      setState(() {
+        activeIndex = idx;
+        onStopPress();
+      });
+    };
+  }
+
   void onStartPress() {
-    timer = Timer.periodic(const Duration(seconds: 1), onTick);
+    timer = Timer.periodic(const Duration(seconds: 1), onTick); // set timer
     setState(() {
       isRunning = true;
     });
   }
 
-  void onStopPress() {
+  void onPausePress() {
     timer.cancel();
     setState(() {
+      isRunning = false;
+    });
+  }
+
+  void onStopPress() {
+    timer.cancel(); // cancel timer
+    setState(() {
+      totalSeconds = timeList[activeIndex];
       isRunning = false;
     });
   }
@@ -45,25 +85,76 @@ class _HomeScreenState extends State<HomeScreen> {
           Flexible(
               flex: 1,
               child: Container(
+                decoration: const BoxDecoration(color: Color(0x44ffffff)),
                 alignment: Alignment.bottomCenter,
-                child: Text(
-                  '$totalSeconds',
-                  style: TextStyle(
-                      color: Theme.of(context).cardColor,
-                      fontSize: 90,
-                      fontWeight: FontWeight.w600),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: onTabPress(0),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: activeIndex == 0
+                                  ? const Color(0x00ffffff)
+                                  : const Color(0x44ffffff),
+                              textStyle: const TextStyle(fontSize: 20)),
+                          child: const Text('Pomodoro'),
+                        ),
+                        ElevatedButton(
+                            onPressed: onTabPress(1),
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: activeIndex == 1
+                                    ? const Color(0x00ffffff)
+                                    : const Color(0x44ffffff),
+                                textStyle: const TextStyle(fontSize: 20)),
+                            child: const Text('Short Break')),
+                        ElevatedButton(
+                            onPressed: onTabPress(2),
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: activeIndex == 2
+                                    ? const Color(0x00ffffff)
+                                    : const Color(0x44ffffff),
+                                textStyle: const TextStyle(fontSize: 20)),
+                            child: const Text('Long Break'))
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      format(totalSeconds),
+                      style: TextStyle(
+                          color: Theme.of(context).cardColor,
+                          fontSize: 90,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ],
                 ),
               )), // UI를 비율 기반으로 더 유연하게 만들어줌,
           Flexible(
               flex: 2,
               child: Center(
-                child: IconButton(
-                  icon: Icon(isRunning
-                      ? Icons.stop_circle_outlined
-                      : Icons.play_circle_outline),
-                  iconSize: 100,
-                  color: Theme.of(context).cardColor,
-                  onPressed: isRunning ? onStopPress : onStartPress,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: Icon(isRunning
+                          ? Icons.pause_circle_outline
+                          : Icons.play_circle_outline),
+                      iconSize: 100,
+                      color: Theme.of(context).cardColor,
+                      onPressed: isRunning ? onPausePress : onStartPress,
+                    ),
+                    if (isRunning)
+                      IconButton(
+                        onPressed: onStopPress,
+                        icon: const Icon(Icons.stop_circle_outlined),
+                        iconSize: 100,
+                        color: Theme.of(context).cardColor,
+                      ),
+                  ],
                 ),
               )),
           Flexible(
