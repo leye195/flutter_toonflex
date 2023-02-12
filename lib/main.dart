6a01b43f487b36f13cf386f98a95139a27b03c3b
leye195 <leye195@naver.com>
2023-02-12 11:56:47 +0900

    feat: lifeCycle, state

--- lib/main.dart
+++ lib/main.dart
@@ -14,33 +14,83 @@ class App extends StatefulWidget {
 // state
 class _AppState extends State<App> {
   int counter = 0;
+  List<int> numbers = [];
+  bool showTitle = true;
 
   void onClicked() {
     setState(() {
-      counter = counter + 1;
+      numbers.add(numbers.length);
+    });
+  }
+
+  void onToggle() {
+    setState(() {
+      showTitle = !showTitle;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            const Text('Click Count',
-                style: TextStyle(
-                  fontSize: 32,
-                )),
-            Text('$counter', style: const TextStyle(fontSize: 30)),
-            IconButton(
-                iconSize: 40,
-                onPressed: onClicked,
-                icon: const Icon(
-                  Icons.add_box_rounded,
-                ))
-          ]),
-        ),
-      ),
-    );
+        theme: ThemeData(
+            textTheme: const TextTheme(
+                titleLarge: TextStyle(color: Colors.red))), // visual properties
+        home: Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Click Count',
+                    style: TextStyle(
+                      fontSize: 32,
+                    )),
+                for (var n in numbers) Text('$n'),
+                IconButton(
+                    iconSize: 40,
+                    onPressed: onClicked,
+                    icon: const Icon(
+                      Icons.add_box_rounded,
+                    )),
+                showTitle ? const LargeTitle() : const Text('Nothing here'),
+                IconButton(
+                    onPressed: onToggle, icon: const Icon(Icons.remove_red_eye))
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class LargeTitle extends StatefulWidget {
+  const LargeTitle({
+    super.key,
+  });
+
+  @override
+  State<LargeTitle> createState() => _LargeTitleState();
+}
+
+class _LargeTitleState extends State<LargeTitle> {
+  @override
+  void initState() {
+    // 부모 요소에 의존하는 데이터를 초기화 해야하는 경우
+    super.initState();
+    print('initState here');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build here');
+    return Text('Large Title',
+        style: TextStyle(
+            fontSize: 32,
+            color: Theme.of(context).textTheme.titleLarge?.color));
+  }
+
+  @override
+  void dispose() {
+    // 위젯이 스크린에서 제거될 때 호출
+    super.dispose();
+    print('dispose here');
   }
 }
