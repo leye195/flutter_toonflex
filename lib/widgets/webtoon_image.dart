d3b83a6cd98598ccbfd0db9cc4257ebc5165ebc0
leye195 <leye195@naver.com>
2023-02-13 13:20:15 +0900

    feat: wrapped image with hero widget

--- lib/widgets/webtoon_image.dart
+++ lib/widgets/webtoon_image.dart
@@ -3,25 +3,30 @@ import 'package:flutter/material.dart';
 class WebtoonImage extends StatelessWidget {
   const WebtoonImage({
     super.key,
+    required this.id,
     required this.thumb,
   });
 
+  final String id;
   final String thumb;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      clipBehavior: Clip.hardEdge,
-      decoration:
-          BoxDecoration(borderRadius: BorderRadius.circular(16), boxShadow: [
-        BoxShadow(
-            blurRadius: 12,
-            offset: const Offset(0, 5),
-            color: Colors.black.withOpacity(0.65)),
-      ]),
-      width: 240,
-      child: Image.network(
-        thumb,
+    return Hero(
+      tag: id,
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration:
+            BoxDecoration(borderRadius: BorderRadius.circular(16), boxShadow: [
+          BoxShadow(
+              blurRadius: 12,
+              offset: const Offset(0, 5),
+              color: Colors.black.withOpacity(0.65)),
+        ]),
+        width: 240,
+        child: Image.network(
+          thumb,
+        ),
       ),
     );
   }
