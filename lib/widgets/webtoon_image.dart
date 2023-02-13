7daa64b33109a59d1bf32876098af93413f62d70
leye195 <leye195@naver.com>
2023-02-13 18:26:23 +0900

    feat:add fit,borderRadius property on WebtoonImage

--- lib/widgets/webtoon_image.dart
+++ lib/widgets/webtoon_image.dart
@@ -5,8 +5,12 @@ class WebtoonImage extends StatelessWidget {
     super.key,
     required this.id,
     required this.thumb,
+    this.fit,
+    this.borderRadius,
   });
 
+  final BoxFit? fit;
+  final BorderRadiusGeometry? borderRadius;
   final String id;
   final String thumb;
 
@@ -16,16 +20,18 @@ class WebtoonImage extends StatelessWidget {
       tag: id,
       child: Container(
         clipBehavior: Clip.hardEdge,
-        decoration:
-            BoxDecoration(borderRadius: BorderRadius.circular(16), boxShadow: [
-          BoxShadow(
-              blurRadius: 12,
-              offset: const Offset(0, 5),
-              color: Colors.black.withOpacity(0.65)),
-        ]),
+        decoration: BoxDecoration(
+            borderRadius: borderRadius ?? BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(
+                  blurRadius: 12,
+                  offset: const Offset(0, 5),
+                  color: Colors.black.withOpacity(0.65)),
+            ]),
         width: 240,
         child: Image.network(
           thumb,
+          fit: fit,
         ),
       ),
     );
